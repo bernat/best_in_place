fa1acfa57f6174a0fc3fa3ecaef18f70e0be2f87
BM5k <me@bm5k.com>
2011-11-29 17:12:15 -0700

    stay consistent with the project's style

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -23,7 +23,7 @@ module BestInPlace
       out = "<span class='best_in_place'"
       out << " id='#{BestInPlace::Utils.build_best_in_place_id(object, field)}'"
       out << " data-url='#{opts[:path].blank? ? url_for(object).to_s : url_for(opts[:path])}'"
-      out << " data-object='#{ class_name_for object }'"
+      out << " data-object='#{ class_name_for(object) }'"
       out << " data-collection='#{collection}'" unless collection.blank?
       out << " data-attribute='#{field}'"
       out << " data-activator='#{opts[:activator]}'" unless opts[:activator].blank?

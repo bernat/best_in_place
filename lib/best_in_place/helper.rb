319f7108c9fef43ab04d7f9cc83eaa04eebc4627
helsayed <hussein.elsayed@badrit.com>
2015-09-14 14:54:58 +0200

    handle time display

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -45,6 +45,7 @@ module BestInPlace
       options[:data]['bip-cancel-button'] = opts[:cancel_button].presence
       options[:data]['bip-cancel-button-class'] = opts[:cancel_button_class].presence
       options[:data]['bip-original-content'] = html_escape(opts[:value] || value).presence
+      options[:data]['bip-original-content'] = options[:data]['bip-original-content'].to_time(:utc).strftime("%H:%M") if opts[:as] == :time
 
       options[:data]['bip-skip-blur'] = opts.has_key?(:skip_blur) ? opts[:skip_blur].presence : BestInPlace.skip_blur
 
